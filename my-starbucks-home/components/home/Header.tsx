6f3c9265287e8ed59ada254c6c58150aa52d9be0
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-21 16:13:18 +0900

    Design: 헤더 컴포넌트 서브 메뉴 및 검색창 UI 제작

--- my-starbucks-home/components/home/Header.tsx
+++ my-starbucks-home/components/home/Header.tsx
@@ -1,6 +1,7 @@
 import styles from '@/styles/import/Header.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
+import { CgSearch } from 'react-icons/cg';
 
 function Header() {
   return (
@@ -17,6 +18,26 @@ function Header() {
               priority
             />
           </Link>
+        </div>
+        <div className={styles['header__sub-menu']}>
+          <ul className={styles['menu']}>
+            <li>
+              <Link href="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link href="#">My Starbucks</Link>
+            </li>
+            <li>
+              <Link href="#">Customer Service & IdeLinks</Link>
+            </li>
+            <li>
+              <Link href="#">Find a Store</Link>
+            </li>
+          </ul>
+          <div className={styles['search']}>
+            <input type="text" />
+            <CgSearch className={styles['icon']} />
+          </div>
         </div>
       </div>
     </header>
