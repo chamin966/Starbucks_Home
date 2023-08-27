99318a5c627065a920f2c4f834dea0e09f4b0f22
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-27 15:09:08 +0900

    Feat: 리저브 커피 섹션 애니메이션 구현

--- my-starbucks-home/components/home/sections/ReserveCoffee.tsx
+++ my-starbucks-home/components/home/sections/ReserveCoffee.tsx
@@ -1,11 +1,38 @@
 import styles from '@/styles/import/ReserveCoffee.module.scss';
+import { gsap } from 'gsap';
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 
 function ReserveCoffee() {
+  const logoRef = useRef<HTMLImageElement | null>(null);
+  const textRef = useRef<HTMLImageElement | null>(null);
+  const imgRef = useRef<HTMLImageElement | null>(null);
+
+  const evalScrollY = () => {
+    let tl = gsap.timeline();
+    if (window.scrollY > 2000) {
+      tl.to(logoRef.current, { duration: 0.4, opacity: 1, x: 100 });
+      tl.to(textRef.current, { duration: 0.5, opacity: 1, x: 100 });
+      tl.to(imgRef.current, { duration: 0.5, opacity: 1, x: 100 });
+    } else {
+      tl.to(logoRef.current, { duration: 0, opacity: 0, x: -50 });
+      tl.to(textRef.current, { duration: 0, opacity: 0, x: 0 });
+      tl.to(imgRef.current, { duration: 0, opacity: 0, x: 200 });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', evalScrollY);
+    return () => {
+      window.removeEventListener('scroll', evalScrollY);
+    };
+  });
+
   return (
     <section className={styles['resserve-coffee']}>
       <div className={styles['resserve-coffee__inner']}>
         <Image
+          ref={logoRef}
           className={styles['inner__logo']}
           src={'/images/reserve_logo.png'}
           alt="리저브 커피 로고"
@@ -13,6 +40,7 @@ function ReserveCoffee() {
           width={152}
         />
         <Image
+          ref={textRef}
           className={styles['inner-text']}
           src={'/images/reserve_text.png'}
           alt="리저브 커피 로고"
@@ -20,6 +48,7 @@ function ReserveCoffee() {
           width={450}
         />
         <Image
+          ref={imgRef}
           className={styles['inner__imgage']}
           src={'/images/reserve_image.png'}
           alt="리저브 커피 로고"
