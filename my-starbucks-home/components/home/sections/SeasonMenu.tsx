aa63e6f9d12120c9efc1d1b509e63dc5c18253a5
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-27 14:06:50 +0900

    Feat: 시즌 메뉴 소개 섹션 애니메이션 제작

--- my-starbucks-home/components/home/sections/SeasonMenu.tsx
+++ my-starbucks-home/components/home/sections/SeasonMenu.tsx
@@ -1,11 +1,50 @@
 import styles from '@/styles/import/SeasonMenu.module.scss';
+import { gsap } from 'gsap';
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 
 function SeasonMenu() {
+  const productImgRef = useRef<HTMLImageElement | null>(null);
+  const textRef = useRef<HTMLImageElement[]>([]);
+
+  const evalScrollY = () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 1750) {
+      gsap.to(productImgRef.current, { opacity: 1, x: 100, duration: 1.5 });
+      textRef.current.forEach((item, index) => {
+        gsap.to(item, {
+          opacity: 1,
+          x: 0,
+          duration: 1,
+        });
+      });
+    } else {
+      gsap.to(productImgRef.current, { opacity: 0, x: 0, duration: 1.5 });
+      textRef.current.forEach((item) => {
+        gsap.to(item, {
+          opacity: 0.5,
+          x: 100,
+        });
+      });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', evalScrollY);
+    return () => {
+      window.removeEventListener('scroll', evalScrollY);
+    };
+  });
+
+  useEffect(() => {
+    evalScrollY();
+  }, []);
+
   return (
     <section className={styles['season-menu']}>
       <div className={styles['season-menu__inner']}>
         <Image
+          ref={productImgRef}
           className={styles['product-image']}
           src="/images/season_product_image.png"
           alt="시즌 메뉴"
@@ -13,6 +52,7 @@ function SeasonMenu() {
           width={493}
         />
         <Image
+          ref={(el) => el && textRef.current.push(el)}
           className={`${styles['text-group']} ${styles['title']}`}
           src="/images/season_product_text1.png"
           alt="시즌 메뉴"
@@ -20,6 +60,7 @@ function SeasonMenu() {
           width={262}
         />
         <Image
+          ref={(el) => el && textRef.current.push(el)}
           className={`${styles['text-group']} ${styles['description']}`}
           src="/images/season_product_text2.png"
           alt="시즌 메뉴"
